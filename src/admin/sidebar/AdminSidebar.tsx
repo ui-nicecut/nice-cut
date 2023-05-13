08991ec3acf71ae92e72e6cec892648bf24aa7df
Haginus <hagiandrei.ah@gmail.com>
2023-05-13 21:23:44 +0300

    show user in sidebar

--- src/admin/sidebar/AdminSidebar.tsx
+++ src/admin/sidebar/AdminSidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './AdminSidebar.scss';
-import { Box, Typography } from '@mui/material';
+import { Avatar, Box, Typography } from '@mui/material';
 import {
   Link,
   useResolvedPath
@@ -13,12 +13,21 @@ export default function AdminSidebar() {
       <div className='logo-wrapper image-wrapper'>
         <img src="/logo.png" alt="logo" className='logo' />
       </div>
-      <Typography variant='subtitle2' sx={{ pl: 1.5, mb: 1, mt: 2 }}>Sections</Typography>
+      <Typography variant='subtitle2' sx={{ pl: 1, mb: 1, mt: 2 }}>Sections</Typography>
       <MenuItem label='Dashboard' to='/admin/dashboard' />
       <MenuItem label='Hair stylists' to='/admin/hair-stylists' />
       <MenuItem label='Services' to='/admin/services' />
       <MenuItem label='Timetable' to='/admin/timetable' />
-
+      <Box sx={{ flexGrow: 1 }} />
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Avatar sx={{ width: 40, height: 40, mr: 1 }}>A</Avatar>
+        <Box sx={{ flexGrow: 1 }}>
+          <Typography variant="subtitle2" component="div">Admin</Typography>
+          <Typography variant="body2" component="div" sx={{ textDecoration: 'underline', cursor: 'pointer' }}>
+            Sign out
+          </Typography>
+        </Box>
+      </Box>
     </div>
   )
 }
