a487ebb3f7d81a4d9095469115e174bf0db167b4
Alexandru Gălăţan <alexcgalatan@gmail.com>
2023-05-14 16:08:23 +0300

    added client side + appointment

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,13 @@ import '@fontsource/roboto/700.css';
 
 import { Routes, Route } from 'react-router-dom';
 import AdminLayout from './admin/admin-layout/AdminLayout';
+import ClientLayout from './client/client-layout/ClientLayout';
 import Dashboard from './admin/dashboard/Dashboard';
 import HairStylists from './admin/hair-stylists/HairStylists';
+import Appointment from './client/appointment/Appointment';
+import AppointmentService from './client/appointmentservice/AppointmentService';
+import AppointmentTime from './client/appointmenttime/AppointmentTime';
+import AppointmentConfirm from './client/appointmentconfirm/AppointmentConfirm';
 
 const App = () => {
   return (
@@ -16,6 +21,12 @@ const App = () => {
       <Route path="admin" element={<AdminLayout />}>
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="hair-stylists" element={<HairStylists />} />
+      </Route>
+      <Route path="client" element={<ClientLayout />}>
+        <Route path="appointment" element={<Appointment />} />
+        <Route path="appointment-service" element={<AppointmentService />} />
+        <Route path="appointment-time" element={<AppointmentTime />} />
+        <Route path="appointment-confirm" element={<AppointmentConfirm />} />
       </Route>
       {/* <Route path="*" element={<NoMatch />} /> */}
     </Routes>
---
+++ src/client/appointmentconfirm/AppointmentConfirm.tsx
@@ -0,0 +1,104 @@
+import React, { useMemo, useState } from 'react';
+import './AppointmentConfirm.scss';
+import { Box, Button, Rating, Typography } from '@mui/material';
+import {
+  Link,
+  useResolvedPath
+} from "react-router-dom";
+import classnames from 'classnames';
+
+export default function Dashboard() {
+
+  const data = {
+    profits: 73286,
+    customers: 1024,
+    newCustomers: 114,
+    returningCustomers: 910,
+    appointments: 1196,
+  }
+
+  const numberFormatCurrency = useMemo(() => new Intl.NumberFormat('en-US', { style: 'currency', currency: 'RON' }), []);
+  const numberFormat = useMemo(() => new Intl.NumberFormat('en-US'), []);
+
+  function Barber({ name, rating }: { name: string, rating: number }) {
+    const to = '/client/appointment'
+
+    const url = useResolvedPath(to).pathname;
+    const active = window.location.pathname === url;
+    return (
+      <div className='dashboard-card small'>
+
+
+        <Box sx={{ display: 'flex', alignItems: 'center', mb: 3 }}>
+        <img src="/scissors.png" alt="logo" className='logo' width = "40" height = "40" />
+            <Box sx={{ flexGrow: 1, ml: 2 }} >
+              <Typography variant="subtitle2" component="div">{name}</Typography>
+              <Typography variant="body2" component="div">{rating} / 5</Typography>
+            </Box>
+            <Rating name="half-rating-read" defaultValue={rating} precision={0.5} readOnly />
+          </Box>
+          
+          
+
+
+      </div>
+
+      
+    );
+  }
+
+  function Service({ name, price, duration }: { name: string, price: number, duration: number }) {
+    const to = '/client/appointment-time'
+
+    const url = useResolvedPath(to).pathname;
+    const active = window.location.pathname === url;
+    return (
+      <div className='dashboard-card small'>
+        <div className='card-content'>
+          <Typography variant='h5' component='div'>{name}</Typography>
+          <Typography variant='h6' component='div'>{price} RON - {duration} minutes</Typography>
+
+        </div>
+      </div>
+
+      
+    );
+  }
+
+
+
+  return (
+    <div className='page'>
+      
+
+      <Box sx={{ flexGrow: 1 }}>
+      <div className='dashboard-card small'>
+            <div className='card-content'>
+            <Box sx={{ display: 'flex', alignItems: 'center', mb: 3 }}>
+              <Typography variant='h4' component='div' sx={{ flexGrow: 1 }}>Appointment summary</Typography>
+            </Box>
+            <Box sx={{ display: 'flex', gap: 3, mb: 2 }}>
+              <Barber name = 'Mihai' rating = {4.5}/>
+              <Service name = 'Simple haircut' price = {60} duration = {30}/>
+              <div className='dashboard-card small'>
+                <div className='card-content'>
+                  <Typography variant='h5' component='div'>Date: 12th of July 2023</Typography>
+                  <Typography variant='h6' component='div'>Time: 12:00</Typography>
+                </div>
+              </div>
+            </Box>
+            </div>
+        </div>
+       
+      </Box>
+
+      <Link to={'/client/appointment-time'} className={classnames('menu-item')}>
+          <Typography variant='subtitle2'>Back</Typography>
+        </Link>
+
+        <Link to={'/client/appointment'} className={classnames('menu-item')}>
+          <Typography variant='subtitle2'>Confirm</Typography>
+        </Link>
+    </div>
+  )
+}
